461fcda479dec24cffa69299d1574a7ba2fe9c27
danielavalera <dvaler01@ucm.es>
2024-03-01 01:34:41 +0100

    cambios mejores

--- JUEZ/PRUEBAS/L04/source.cpp
+++ JUEZ/PRUEBAS/L04/source.cpp
@@ -54,6 +54,8 @@
 #include <cassert>
 #include <string> 
 #include <utility>
+#include <queue>
+#include <deque>
 
 
 using namespace std;
@@ -68,252 +70,12 @@ using namespace std;
 
 // Añade los tipos de datos auxiliares y funciones que necesites
 
-/*
- * ---------------------------------------------------
- *                ESTRUCTURAS DE DATOS
- * ---------------------------------------------------
- *              Manuel Montenegro Montes
- *              Facultad de Informática
- *         Universidad Complutense de Madrid
- * ---------------------------------------------------
- */
-
- /*
-  * Implementación del TAD Cola mediante listas enlazadas.
-  */
-
-#ifndef __QUEUE_LINKEDLIST_H
-#define __QUEUE_LINKEDLIST_H
-
-#include <cassert>
-#include <iostream>
-
-template <typename T> class QueueLinkedList {
-public:
-    QueueLinkedList() : front_node(nullptr), back_node(nullptr) {}
-    QueueLinkedList(const QueueLinkedList& other) {
-        copy_nodes_from(other.front_node);
-    }
-
-    ~QueueLinkedList() { free_nodes_from(front_node); }
-
-    QueueLinkedList& operator=(const QueueLinkedList& other) {
-        if (this != &other) {
-            free_nodes_from(front_node);
-            copy_nodes_from(other.front_node);
-        }
-        return *this;
-    }
-
-    void push(const T& elem) {
-        Node* new_node = new Node{ elem, nullptr };
-        if (back_node == nullptr) {
-            back_node = new_node;
-            front_node = new_node;
-        }
-        else {
-            back_node->next = new_node;
-            back_node = new_node;
-        }
-    }
-
-    void pop() {
-        assert(front_node != nullptr);
-        Node* target = front_node;
-        front_node = front_node->next;
-        if (back_node == target) {
-            back_node = nullptr;
-        }
-        delete target;
-    }
-
-    T& front() {
-        assert(front_node != nullptr);
-        return front_node->value;
-    }
-
-    const T& front() const {
-        assert(front_node != nullptr);
-        return front_node->elem;
-    }
-
-    bool empty() const { return (front_node == nullptr); }
-
-private:
-    struct Node {
-        T value;
-        Node* next;
-    };
-
-    void copy_nodes_from(Node* other);
-    void free_nodes_from(Node* other);
-
-    Node* front_node;
-    Node* back_node;
-};
-
-template <typename T> void QueueLinkedList<T>::copy_nodes_from(Node* other) {
-    if (other == nullptr) {
-        front_node = nullptr;
-        back_node = nullptr;
-    }
-    else {
-        front_node = new Node{ other->value, nullptr };
-        back_node = front_node;
-        Node* current = other->next;
-        while (current != nullptr) {
-            Node* new_node = new Node{ current->value, nullptr };
-            back_node->next = new_node;
-            current = current->next;
-            back_node = new_node;
-        }
-    }
-}
-
-template <typename T> void QueueLinkedList<T>::free_nodes_from(Node* other) {
-    Node* current = other;
-    while (current != nullptr) {
-        Node* next = current->next;
-        delete current;
-        current = next;
-    }
-}
-
-
-/*
- * Implementación del TAD Doble Cola utilizando una lista
- * doblemente enlazada con nodo fantasma.
- */
-
-template <typename Elem> class Dequeue {
-private:
-    struct Node {
-        Elem value;
-        Node* next;
-        Node* prev;
-    };
-
-public:
-    Dequeue() {
-        head = new Node;
-        head->next = head;
-        head->prev = head;
-    }
-
-    Dequeue(const Dequeue& other) : Dequeue() { copy_nodes_from(other); }
-
-    ~Dequeue() { delete_nodes(); }
-
-    void push_front(const Elem& elem) {
-        Node* new_node = new Node{ elem, head->next, head };
-        head->next->prev = new_node;
-        head->next = new_node;
-    }
-
-    void push_back(const Elem& elem) {
-        Node* new_node = new Node{ elem, head, head->prev };
-        head->prev->next = new_node;
-        head->prev = new_node;
-    }
-
-    void pop_front() {
-        assert(head->next != head);
-        Node* target = head->next;
-        head->next = target->next;
-        target->next->prev = head;
-        delete target;
-    }
-
-    void pop_back() {
-        assert(head->next != head);
-        Node* target = head->prev;
-        target->prev->next = head;
-        head->prev = target->prev;
-        delete target;
-    }
-
-    bool empty() const { return head->next == head; };
-
-    const Elem& front() const {
-        assert(head->next != head);
-        return head->next->value;
-    }
-
-    Elem& front() {
-        assert(head->next != head);
-        return head->next->value;
-    }
-
-    const Elem& back() const {
-        assert(head->next != head);
-        return head->prev->value;
-    }
-
-    Elem& back() {
-        assert(head->next != head);
-        return head->prev->value;
-    }
-
-    Dequeue& operator=(const Dequeue& other) {
-        if (this != &other) {
-            delete_nodes();
-            head = new Node;
-            head->next = head->prev = head;
-            copy_nodes_from(other);
-        }
-        return *this;
-    }
-
-private:
-    Node* head;
-
-    void delete_nodes();
-    void copy_nodes_from(const Dequeue& other);
-};
-
-template <typename Elem> void Dequeue<Elem>::delete_nodes() {
-    Node* current = head->next;
-    while (current != head) {
-        Node* target = current;
-        current = current->next;
-        delete target;
-    }
-
-    delete head;
-}
-
-template <typename Elem>
-void Dequeue<Elem>::copy_nodes_from(const Dequeue& other) {
-    Node* current_other = other.head->next;
-    Node* last = head;
-
-    while (current_other != other.head) {
-        Node* new_node = new Node{ current_other->value, head, last };
-        last->next = new_node;
-        last = new_node;
-        current_other = current_other->next;
-    }
-    head->prev = last;
-}
-
-#endif
-
-
-
-// Implementa aquí la función para tratar UN caso de prueba. La función
-// devuelve false si, en lugar de encontrarse con un caso de prueba, se ha
-// topado con la marca de fin de fichero. Por el contrario, si se ha
-// encontrado con un caso de prueba y lo ha procesado, devuelve true.
-
-// No olvides indicar y justificar el coste de la función.
-
-int personasPendientes(Dequeue<char>& exs, QueueLinkedList<char>& conds, int t) {
+int personasPendientes(deque<char>& exs, queue<char>& conds, int t) {
     int countPerPendientes = 0;
-    Dequeue<char>::Node* current = exs.head->next;
 
     for (int i = 0; i < t; i++) //según los días (t)
     {
-        while (current->value == 'G' || current->value == 'B' || current->value == 'A') {
+        while (!exs.empty()) {
             char exAux = exs.front();
             char condAux = conds.front();
             exs.pop_front();
@@ -326,7 +88,7 @@ int personasPendientes(Dequeue<char>& exs, QueueLinkedList<char>& conds, int t) 
                 else {
                     conds.pop();
                 }
-                    
+
             }
             /*else if (exAux == 'B') {
 
@@ -346,14 +108,20 @@ int personasPendientes(Dequeue<char>& exs, QueueLinkedList<char>& conds, int t) 
     return countPerPendientes;
 }
 
+// Implementa aquí la función para tratar UN caso de prueba. La función
+// devuelve false si, en lugar de encontrarse con un caso de prueba, se ha
+// topado con la marca de fin de fichero. Por el contrario, si se ha
+// encontrado con un caso de prueba y lo ha procesado, devuelve true.
+
+// No olvides indicar y justificar el coste de la función.
 bool tratar_caso() {
     int n, m, t; cin >> n >> m >> t;
 
     if (!cin) return false;
 
     char examinador, conductor;
-    Dequeue<char> examinadores; //porq nos interesa iterar siempre en la misma lista
-    QueueLinkedList<char> conductores;
+    deque<char> examinadores; //porq nos interesa iterar siempre en la misma lista
+    queue<char> conductores;
 
     for (int i = 0; i < n; i++)
     {
