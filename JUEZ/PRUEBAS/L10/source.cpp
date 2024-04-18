b27c0f44fa636d7996e9a3d4a9b20a1d734365ee
danielavalera <dvaler01@ucm.es>
2024-04-18 18:12:45 +0200

    version casi final

--- JUEZ/PRUEBAS/L10/source.cpp
+++ JUEZ/PRUEBAS/L10/source.cpp
@@ -19,27 +19,48 @@
 #include <fstream>
 #include <cassert>
  // Añade los include que necesites
-#include <sstream>
+#include <unordered_map>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
+//no se pide el coste
 bool tratar_caso() {
 	int caso; cin >> caso;
 	if (caso == 0) return false;
 
-	string nombre;
-	int casilla;
-	vector<int> v;
+	unordered_map<int, vector<string>> carton;
+	unordered_map<string, int> player_count;
+	vector<string> winners;
+
 	for (int i = 0; i < caso; i++){
-		cin >> nombre >> casilla;
-		while (casilla != 0) {
-			v.push_back(casilla);
-			cin >> casilla;
+		string name; cin >> name;
+		//player_count[name] = 0; // inicializo el contador para ese jugador
+		int num;
+		while (cin >> num && num != 0) {
+			carton[num].push_back(name);
+			player_count[name]++;
 		}
 	}
 
+	int num;
+	while (cin >> num && num != 0) {
+		if (carton.find(num) != carton.end(num)) { // si find() devuelve end() significa que no está el número, find() va con end()
+			for (const auto& player: carton[num]){ // itero sobre el vector de nombres
+				player_count[player]--;
+				if (player_count[player] == 0) {
+					winners.push_back(player);
+				}
+			}
+		}
+	}
 	
+	sort(winners.begin(), winners.end());
+	for (const auto& winner:winners){
+		cout << winner << " ";
+	}
+	cout << "\n";
 	
 	
 }
