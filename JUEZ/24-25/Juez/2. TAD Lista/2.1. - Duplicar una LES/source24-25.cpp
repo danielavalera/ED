246b2a614f2e15de2dda1f0ae89ffcd7658ddc8e
danielavalera <dvaler01@ucm.es>
2025-01-27 21:49:09 +0100

    correct

---
+++ JUEZ/24-25/Juez/2. TAD Lista/2.1. - Duplicar una LES/source24-25.cpp
@@ -0,0 +1,269 @@
+/*
+  * ImplementaciÃ³n del TAD Lista mediante listas enlazadas simples.
+  *
+  * Esta versiÃ³n introduce el constructor de copia.
+  */
+
+
+
+#include <cassert>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+using namespace std;
+class ListLinkedSingle {
+private:
+    struct Node {
+        int value;
+        Node* next;
+    };
+
+public:
+    ListLinkedSingle() : head(nullptr) { }
+    ListLinkedSingle(vector<int> v) {
+        if (v.size() > 0) {
+            Node* node = new Node;
+            Node* next_node = new Node;
+            node->value = v[0];
+            this->head = node;
+            for (int i = 1; i < v.size(); i++) {
+                next_node = new Node;
+                next_node->value = v[i];
+                node->next = next_node;
+                node = next_node;
+            }
+            node->next = nullptr; // para el ultimo elemento
+        }
+        else {
+            this->head = nullptr;
+        }
+    }
+    ~ListLinkedSingle() {
+        delete_list(head);
+    }
+
+    ListLinkedSingle(const ListLinkedSingle& other)
+        : head(copy_nodes(other.head)) { }
+
+    void duplicate();
+
+    void push_front(const int& elem) {
+        Node* new_node = new Node;
+        new_node->value = elem;
+        new_node->next = head;
+        head = new_node;
+    }
+
+    void push_back(const int& elem);
+
+    void pop_front() {
+        assert(head != nullptr);
+        Node* old_head = head;
+        head = head->next;
+        delete old_head;
+    }
+
+    void pop_back();
+
+    int size() const;
+
+    bool empty() const {
+        return head == nullptr;
+    };
+
+    const int& front() const {
+        assert(head != nullptr);
+        return head->value;
+    }
+
+    int& front() {
+        assert(head != nullptr);
+        return head->value;
+    }
+
+    const int& back() const {
+        return last_node()->value;
+    }
+
+    int& back() {
+        return last_node()->value;
+    }
+
+    void invertir(Node* node, Node* next);
+    const int& at(int index) const {
+        Node* result_node = nth_node(index);
+        assert(result_node != nullptr);
+        return result_node->value;
+    }
+
+    int& at(int index) {
+        Node* result_node = nth_node(index);
+        assert(result_node != nullptr);
+        return result_node->value;
+    }
+
+    void display(std::ostream& out) const;
+    void display() const {
+        display(std::cout);
+    }
+    Node* head;
+
+private:
+    void delete_list(Node* start_node);
+    Node* last_node() const;
+    Node* nth_node(int n) const;
+    Node* copy_nodes(Node* start_node) const;
+
+};
+
+ListLinkedSingle::Node* ListLinkedSingle::copy_nodes(Node* start_node) const {
+    if (start_node != nullptr) {
+        Node* result = new Node;
+        result->value = start_node->value;
+        result->next = copy_nodes(start_node->next);
+        return result;
+    }
+    else {
+        return nullptr;
+    }
+}
+
+void ListLinkedSingle::delete_list(Node* start_node) {
+    if (start_node != nullptr) {
+        delete_list(start_node->next);
+        delete start_node;
+    }
+}
+
+void ListLinkedSingle::push_back(const int& elem) {
+    Node* new_node = new Node;
+    new_node->value = elem;
+    new_node->next = nullptr;
+    if (head == nullptr) {
+        head = new_node;
+    }
+    else {
+        last_node()->next = new_node;
+    }
+}
+
+void ListLinkedSingle::pop_back() {
+    assert(head != nullptr);
+    if (head->next == nullptr) {
+        delete head;
+        head = nullptr;
+    }
+    else {
+        Node* previous = head;
+        Node* current = head->next;
+
+        while (current->next != nullptr) {
+            previous = current;
+            current = current->next;
+        }
+
+        delete current;
+        previous->next = nullptr;
+    }
+}
+
+int ListLinkedSingle::size() const {
+    int num_nodes = 0;
+
+    Node* current = head;
+    while (current != nullptr) {
+        num_nodes++;
+        current = current->next;
+    }
+
+    return num_nodes;
+}
+
+
+ListLinkedSingle::Node* ListLinkedSingle::last_node() const {
+    assert(head != nullptr);
+    Node* current = head;
+    while (current->next != nullptr) {
+        current = current->next;
+    }
+    return current;
+}
+
+ListLinkedSingle::Node* ListLinkedSingle::nth_node(int n) const {
+    assert(0 <= n);
+    int current_index = 0;
+    Node* current = head;
+
+    while (current_index < n && current != nullptr) {
+        current_index++;
+        current = current->next;
+    }
+
+    return current;
+}
+
+void ListLinkedSingle::display(std::ostream& out) const {
+    out << "[";
+    if (head != nullptr) {
+        out << head->value;
+        Node* current = head->next;
+        while (current != nullptr) {
+            out << ", " << current->value;
+            current = current->next;
+        }
+    }
+    out << "]";
+}
+
+void ListLinkedSingle::duplicate() {
+    Node* current = head;
+    while (current != nullptr) {
+        Node* aux = new Node;
+        aux->value = current->value;
+        aux->next = current->next;
+        current->next = aux;
+        current = current->next->next;
+    }
+}
+
+
+bool tratar_caso() {
+    int elem;
+    cin >> elem;
+    if (!cin)
+        return false;
+    vector<int> v;
+    while (elem != 0) {
+        v.push_back(elem);
+        cin >> elem;
+    }
+    ListLinkedSingle lista(v);
+    //A partir de aqui, la lista esta inicializada
+    lista.duplicate();
+    //cout << "\n";
+    lista.display();
+    
+    cout << "\n";
+    return true;
+}
+
+int main() {
+#ifndef DOMJUDGE
+    std::ifstream in("2.1.txt");
+    auto cinbuf = std::cin.rdbuf(in.rdbuf());
+#endif
+    // La entrada comienza con el número de casos de prueba.
+    int num_casos;
+    cin >> num_casos;
+
+    for (int i = 0; i < num_casos; i++) {
+        tratar_caso();
+    }
+
+#ifndef DOMJUDGE
+    std::cin.rdbuf(cinbuf);
+#endif
+
+    return 0;
+}
